b596329fd26ce5099b2e0456ea5419b591ea2eca
ulic75 <ulic75@gmail.com>
2023-01-19 16:01:59 -0800

    fix: use environment vars
    VITE_ vars can be exposed to the client, process.env vars are protected against that

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig(() => {
   return {

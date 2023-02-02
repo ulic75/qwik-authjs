8d792cc3b0f7e9d9ffca2af3e6933a54d571a17d
ulic75 <ulic75@gmail.com>
2023-02-02 08:15:11 -0700

    fix: provider typing (#10)

--- src/routes/api/auth/[...authjs]/index!.ts
+++ src/routes/api/auth/[...authjs]/index!.ts
@@ -1,3 +1,4 @@
+import { Provider } from '@auth/core/providers';
 import Auth0 from '@auth/core/providers/auth0';
 import Github from '@auth/core/providers/github';
 import Credentials from '@auth/core/providers/credentials';
@@ -6,7 +7,6 @@ import { QwikCityAuth } from '~/lib/frameworks-qwik';
 
 export const { onRequest } = QwikCityAuth({
   providers: [
-    //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
     Credentials({
       name: 'Email',
       credentials: {
@@ -20,16 +20,14 @@ export const { onRequest } = QwikCityAuth({
         return null;
       },
     }),
-    //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
     Auth0({
       clientId: process.env.AUTH0_ID || '',
       clientSecret: process.env.AUTH0_SECRET || '',
       issuer: process.env.AUTH0_ISSUER,
     }),
-    //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
     Github({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
-  ],
+  ] as Provider[],
 });
